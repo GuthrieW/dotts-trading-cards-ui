1bcb8e5b6a7c3bfbc7b13f2aab4fd7c595589697
GuthrieW <wesleyeguthrie@gmail.com>
2020-06-16 16:49:59 -0500

    Fixed API URL

--- common/api/api-url.js
+++ common/api/api-url.js
@@ -1,10 +1,10 @@
 // TODO: Get rid of this hacky way of having environment variables.
 
 /* localhost */
-export const API_URL = 'http://localhost:8080';
+// export const API_URL = 'http://localhost:8080';
 
 /* development */
 // export const API_URL = '';
 
 /* production */
-// export const API_URL = 'https://dotts-trading-cards-api.herokuapp.com';
+export const API_URL = 'https://dotts-trading-cards-api.herokuapp.com';
