5547be3366d6d82df6f9cc7c605e7096f41dc39d
GuthrieW <wesleyeguthrie@gmail.com>
2020-06-16 16:56:52 -0500

    Fixed layout import

--- components/card.js
+++ components/card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from './Layout';
+import Layout from './layout';
 import { withRouter } from 'next/router';
 import Swal from 'sweetalert';
 import { Status } from '/dotts-trading-cards-ui/common/api/http-status';
