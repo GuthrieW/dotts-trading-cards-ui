a52e4043b6ceb60a3708569c1bcd277b17cd5826
Wesley Guthrie <wesleyeguthrie@gmail.com>
2020-06-19 15:35:22 -0500

    Use images instead of buttons on homepage (#21)
    * Added new images
    
    * Fixed imports
    
    * Added column widths
    
    * Changed images to cards
    
    * Added margins to cards
    
    * Fixed path name

--- components/home.js
+++ components/home.js
@@ -1,60 +1,77 @@
 import React from 'react';
-import { Row } from 'reactstrap';
+import {
+	Row,
+	Col,
+	Container,
+	Card,
+	CardImg,
+	CardBody,
+	CardTitle,
+} from 'reactstrap';
+import Router from 'next/router';
 import Layout from './layout';
 
 export default class Home extends React.Component {
 	constructor() {
 		super();
 
-		this.bind;
+		this.handleOnClick = this.handleOnClick.bind(this);
+	}
+
+	handleOnClick(route) {
+		Router.push({
+			pathname: route,
+		});
 	}
 
 	render() {
 		return (
 			<Layout title='Home'>
-				<Row
-					style={{
-						display: 'grid',
-						justifyContent: 'center',
-						alignItems: 'center',
-						marginBottom: '5px',
-					}}
-				>
-					<a className='btn btn-primary' href='/open-packs'>
-						<span style={{ color: 'white' }}>Open Packs</span>
-					</a>
-				</Row>
-				<Row
-					style={{
-						display: 'grid',
-						justifyContent: 'center',
-						alignItems: 'center',
-						marginBottom: '5px',
-					}}
-				>
-					<a
-						className='btn btn-primary'
-						href='/collection/my-collection'
-					>
-						<span style={{ color: 'white' }}>
-							View Your Collection
-						</span>
-					</a>
-				</Row>
-				<Row
-					style={{
-						display: 'grid',
-						justifyContent: 'center',
-						alignItems: 'center',
-						marginBottom: '5px',
-					}}
-				>
-					<a className='btn btn-primary' href='/player-list'>
-						<span style={{ color: 'white' }}>
-							View Other Members' Collections
-						</span>
-					</a>
-				</Row>
+				<Container>
+					<Row>
+						<Col>
+							<Card className='ml-1 mr-1'>
+								<CardImg
+									onClick={() => {
+										this.handleOnClick(`/open-packs`);
+									}}
+									src='https://i.imgur.com/e2wFkJp.png'
+								/>
+								<CardBody>
+									<CardTitle>Open Packs</CardTitle>
+								</CardBody>
+							</Card>
+						</Col>
+						<Col>
+							<Card className='ml-1 mr-1'>
+								<CardImg
+									onClick={() => {
+										this.handleOnClick(
+											`/collection/my-collection`
+										);
+									}}
+									src='https://i.imgur.com/JRQQptU.png'
+								/>
+								<CardBody>
+									<CardTitle>My Collection</CardTitle>
+								</CardBody>
+							</Card>
+						</Col>
+						<Col>
+							<Card className='ml-1 mr-1'>
+								<CardImg
+									onClick={() => {
+										this.handleOnClick(`/player-list`);
+									}}
+									src='https://i.imgur.com/Y7PQ3sj.png'
+								/>
+								<CardBody>
+									<CardTitle>Other Collections</CardTitle>
+								</CardBody>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
 			</Layout>
 		);
 	}
