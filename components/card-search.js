8254a41a76ca4ca35157245f2b9827d6923c498c
GuthrieW <wesleyeguthrie@gmail.com>
2020-07-27 13:01:53 -0500

    Added logging back in

--- components/card-search.js
+++ components/card-search.js
@@ -73,6 +73,7 @@ export default class CardSearch extends React.Component {
 
 		await callApi(url, method)
 			.then((response) => {
+				console.log(response);
 				if (response.status === Status.OK) {
 					this.setState({
 						cards: response.data,
