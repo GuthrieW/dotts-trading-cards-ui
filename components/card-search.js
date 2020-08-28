09585cafc2a9b6c2963396b42f977496884584c3
GuthrieW <wesleyeguthrie@gmail.com>
2020-08-27 22:10:15 -0500

    Fixed onclick

--- components/card-search.js
+++ components/card-search.js
@@ -155,7 +155,9 @@ export default class CardSearch extends React.Component {
 											maxHeight: '504px',
 											margin: '2px',
 										}}
-										onClick={redirectToEditCard(card._id)}
+										onClick={() => {
+											redirectToEditCard(card._id);
+										}}
 										src={card.image_url}
 									/>
 								</div>
