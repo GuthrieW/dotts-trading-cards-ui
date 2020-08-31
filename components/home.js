09ce641d8ca6158e343f6a4bec4d8441cba28b3e
GuthrieW <wesleyeguthrie@gmail.com>
2020-08-31 11:57:28 -0500

    Add slider to homepage

--- components/home.js
+++ components/home.js
@@ -10,12 +10,39 @@ import {
 } from 'reactstrap';
 import Router from 'next/router';
 import Layout from './layout';
+import Slider from 'react-slick';
 
 export default class Home extends React.Component {
 	constructor() {
 		super();
+		this.state = {
+			'cards-to-display': [],
+		};
 
 		this.handleOnClick = this.handleOnClick.bind(this);
+	}
+
+	async componentDidMount() {
+		const url = `${API_URL}/cards`;
+		const method = Method.GET;
+
+		await callApi(url, method)
+			.then((response) => {
+				console.log(response);
+				if (response.status === Status.OK) {
+					this.setState({
+						'cards-to-display': response.data,
+					});
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+				Swal({
+					title: 'Server Error',
+					text: 'The server encountered an error',
+					icon: 'error',
+				});
+			});
 	}
 
 	handleOnClick(route) {
@@ -66,6 +93,53 @@ export default class Home extends React.Component {
 								</CardBody>
 							</Card>
 						</CardDeck>
+					</Row>
+					<Row>
+						<Slider
+							{...{
+								lazyLoad: 'ondemand',
+								slidesToShow: 3,
+								slidesToScroll: 1,
+								nextArrow: <SampleNextArrow />,
+								prevArrow: <SamplePrevArrow />,
+								className: 'center',
+								infinite: false,
+								speed: 500,
+								responsive: [
+									{
+										breakpoint: 1512,
+										settings: {
+											slidesToShow: 2,
+										},
+									},
+									{
+										breakpoint: 1008,
+										settings: {
+											slidesToShow: 1,
+										},
+									},
+								],
+							}}
+						>
+							{this.state['cards'].map((card, index) => (
+								<div style={{ maxWidth: '33%' }} key={index}>
+									<img
+										style={{
+											maxHeight: '504px',
+											margin: '2px',
+										}}
+										src={card.image_url}
+									/>
+									<Button
+										onClick={() => {
+											this.redirectToEditCard(card._id);
+										}}
+									>
+										Edit Card
+									</Button>
+								</div>
+							))}
+						</Slider>
 					</Row>
 				</Container>
 			</Layout>
