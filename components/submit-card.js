5c2ac6a4292e57569aec1d1246c3eb83c1807d16
GuthrieW <wesleyeguthrie@gmail.com>
2020-09-08 22:07:27 -0500

    Kept default behavior

--- components/submit-card.js
+++ components/submit-card.js
@@ -36,6 +36,7 @@ class SubmitCard extends React.Component {
 			'card-rarity': RARITY_LEVELS.DEFAULT,
 			'card-image-url': '',
 			'displayImage': false,
+			'is-loading': false,
 		};
 
 		this.handleChange = this.handleChange.bind(this);
@@ -65,8 +66,6 @@ class SubmitCard extends React.Component {
 	}
 
 	async handleSubmit(event) {
-		event.preventDefault();
-
 		if (this.isEmptyString(this.state['nsfl-username'])) {
 			Swal({
 				title: 'Submission Rejected',
@@ -249,7 +248,11 @@ class SubmitCard extends React.Component {
 									onChange={this.handleChange}
 								/>
 							</FormGroup>
-							<Button color='primary' type='submit'>
+							<Button
+								color='primary'
+								type='submit'
+								disabled={this.state['is-loading']}
+							>
 								Submit
 							</Button>
 						</Form>
